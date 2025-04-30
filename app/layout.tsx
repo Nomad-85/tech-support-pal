38b4e817981d14e6523a0b6e556b5c241e422375
Nomad-85 <75000931+Nomad-85@users.noreply.github.com>
2025-04-30 13:05:34 -0500

    Update layout.tsx
    - favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
           {children}
