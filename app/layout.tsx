5f98313d095fb266df62cf7c88117cffb6d0cd01
Nomad-85 <75000931+Nomad-85@users.noreply.github.com>
2025-04-30 13:23:20 -0500

    -fixing favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/public/favicon.png" type="image/png" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
