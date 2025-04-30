9d05acdc15ddfbea0b6f6a8757683f5f337926e7
Nomad-85 <75000931+Nomad-85@users.noreply.github.com>
2025-04-30 12:45:48 -0500

    - manual comment out testimonials
    - fixed problems, maybe

--- app/page.tsx
+++ app/page.tsx
@@ -195,7 +195,8 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="w-full py-12 md:py-24 lg:py-32">
+        {/* comment out Testimonials */}
+        {/* <section className="w-full py-12 md:py-24 lg:py-32">
           <div className="container px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 items-center">
               <div className="space-y-4">
@@ -222,7 +223,7 @@ export default function Home() {
               />
             </div>
           </div>
-        </section>
+        </section> */}
 
         <section className="w-full py-12 md:py-24 lg:py-32 bg-primary text-white">
           <div className="container px-4 md:px-6">
